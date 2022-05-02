f4c3ca01c43dd879ed77c0446bc53b4fccdb1d40
PlaustralCL <plaustralcl@gmail.com>
2022-05-01 20:26:42 -0400

    Show available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,13 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| [airport.code, airport.id] }
     @flights = Flight.new
+    @flight_info = params[:flight].present? ? available_flights : ""
+  end
+
+
+  private
+
+  def available_flights
+    Flight.where("departure_airport_id = ?", params[:flight][:departure_airport_id]).and(Flight.where("arrival_airport_id = ?", params[:flight][:arrival_airport_id])).order(:departure_time)
   end
 end
