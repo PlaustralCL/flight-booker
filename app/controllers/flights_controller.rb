d4c33119483a76b42e343fb3bae6776c5367093b
PlaustralCL <plaustralcl@gmail.com>
2022-05-02 07:40:51 -0400

    Keep airport choices after submitting search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
     @airport_options = Airport.all.order(:code).map { |airport| [airport.code, airport.id] }
     @flights = Flight.new
     @flight_info = params[:flight].present? ? available_flights : ""
+    @selected_airports = target_aiports
   end
 
 
@@ -14,6 +15,14 @@ class FlightsController < ApplicationController
     else
       flights = Flight.where("departure_airport_id = ?", params[:flight][:departure_airport_id]).and(Flight.where("arrival_airport_id = ?", params[:flight][:arrival_airport_id]))
       flights.order(:arrival_time)
+    end
+  end
+
+  def target_aiports
+    if params[:flight].present?
+      { departure_airport_id: params[:flight][:departure_airport_id], arrival_airport_id: params[:flight][:arrival_airport_id] }
+    else
+      { departure_airport_id: "1", arrival_airport_id: "4"}
     end
   end
 end
