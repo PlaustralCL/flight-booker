56c1914689f01b3bbad21255310d5775910b2023
PlaustralCL <plaustralcl@gmail.com>
2022-05-03 19:59:06 -0400

    Move Flights query to model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,23 +2,12 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| [airport.code, airport.id] }
     @flights = Flight.new
-    @search_results = available_flights
+    @search_results = Flight.new.available_flights(params)
     @search_criteria = search_criteria
   end
 
 
   private
-
-  def available_flights
-    return "" unless params[:flight].present?
-
-    if (params[:flight][:departure_airport_id] == params[:flight][:arrival_airport_id]) || params[:flight][:departure_airport_id] == "" || params[:flight][:arrival_airport_id] == ""
-      ""
-    else
-      flights = Flight.where("departure_airport_id = ?", params[:flight][:departure_airport_id]).and(Flight.where("arrival_airport_id = ?", params[:flight][:arrival_airport_id]))
-      flights.order(:arrival_time)
-    end
-  end
 
   def search_criteria
     if params[:flight].present?
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,18 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+
+
+  def available_flights(params)
+    return "" unless params[:flight].present?
+
+    if (params[:flight][:departure_airport_id] == params[:flight][:arrival_airport_id])
+      ""
+    else
+      Flight.where("departure_airport_id = ?", params[:flight][:departure_airport_id])
+            .where("arrival_airport_id = ?", params[:flight][:arrival_airport_id])
+            .order(:departure_time)
+    end
+  end
 end
