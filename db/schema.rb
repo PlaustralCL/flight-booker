8947c169025c7a9294e3ae31ab97af97594dbde9
PlaustralCL <plaustralcl@gmail.com>
2022-05-04 07:24:31 -0400

    Generate Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_110737) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_112241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_110737) do
     t.string "state"
     t.string "name"
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
