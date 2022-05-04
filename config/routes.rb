364cf9664363088c46375637ae0b1f222aef4c0c
PlaustralCL <plaustralcl@gmail.com>
2022-05-04 07:28:56 -0400

    Generate Bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :flights, only: [:index]
+  resources :bookings, only: [:new]
 
   root "flights#index"
 end
