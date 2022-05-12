adccb25506e1adc44832c371607f6d8fcb49e43b
PlaustralCL <plaustralcl@gmail.com>
2022-05-11 21:00:09 -0400

    Format the flight info display on search results

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -5,9 +5,22 @@ module FlightsHelper
     end
   end
 
+  # def format_flight(flight)
+  #   "#{flight.departure_time.localtime.strftime("%H:%M %p")} departure from "\
+  #   "#{flight.departure_airport.code} with a #{flight.arrival_time.localtime.strftime("%H:%M %p")} "\
+  #   "arrival at #{flight.arrival_airport.code}"
+  # end
+
   def format_flight(flight)
-    "#{flight.departure_time.localtime.strftime("%H:%M %p")} departure from "\
-    "#{flight.departure_airport.code} with a #{flight.arrival_time.localtime.strftime("%H:%M %p")} "\
-    "arrival at #{flight.arrival_airport.code}"
+    "Depart: #{flight_departure(flight)} -- Arrive: #{flight_arrival(flight)}"
+
+  end
+
+  def flight_departure(flight)
+    "#{flight.departure_airport.city} (#{flight.departure_airport.code}) at #{flight.departure_time.localtime.strftime("%H:%M %p")}"
+  end
+
+  def flight_arrival(flight)
+    "#{flight.arrival_airport.city} (#{flight.arrival_airport.code}) at #{flight.arrival_time.localtime.strftime("%H:%M %p")}"
   end
 end
